b5c1170622251844f41b74249ae0daf5d27402aa
jwaine44 <jwaine44@gmail.com>
2022-06-22 17:54:52 -0400

    Initialize Repo

---
+++ OddOccurrencesinArray.js
@@ -0,0 +1,42 @@
+/* 
+Given a non-empty array of odd length containing ints where every int but one
+has a matching pair (another int that is the same)
+return the only int that has no matching pair.
+*/
+
+const nums1 = [1];
+const expected1 = 1;
+
+const nums2 = [5, 4, 5];
+const expected2 = 4;
+
+const nums3 = [5, 4, 3, 4, 3, 4, 5];
+const expected3 = 4; // there is a pair of 4s but one 4 has no pair.
+
+const nums4 = [5, 2, 6, 2, 3, 1, 6, 3, 2, 5, 2];
+const expected4 = 1;
+
+function oddOccurrencesInArray(nums) {
+    var expected = {};
+    for(var i = 0; i < nums.length; i++) {
+        if(expected.hasOwnProperty(nums[i])){       // Checks to see if expected has repeated instances of nums[i]
+            expected[nums[i]]++;
+        }
+        else {
+            expected[nums[i]] = 1;
+        }
+    }
+        for(key in expected) {
+            if(expected[key] % 2 == 1){
+                return key;
+            }
+            else {
+                continue;
+            }
+        }
+}
+
+console.log(oddOccurrencesInArray(nums1), "should equal", expected1);
+console.log(oddOccurrencesInArray(nums2), "should equal", expected2);
+console.log(oddOccurrencesInArray(nums3), "should equal", expected3);
+console.log(oddOccurrencesInArray(nums4), "should equal", expected4);
